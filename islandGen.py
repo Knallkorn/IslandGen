1c5521ed560aca407245c01a6b6e6dc95f689558
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-03-15 11:46:52 +1100

    Cleaned Up Code
    Added comments, removed unnecessary lines, looped trees so they work properly on larger scales, moved circular gradient to start, moved noise application into first main[] loop

--- islandGen.py
+++ islandGen.py
@@ -1,7 +1,6 @@
 #Import libraries
 
 import random
-import keyboard
 import os
 import noise
 import numpy
@@ -29,7 +28,7 @@ rockCol = [209, 209, 209]
 grassCol = [37, 170, 77]
 dgrassCol = [34, 161, 63]
 treeCol = [10, 122, 42]
-dirtCol = [74, 62, 36]
+mountCol = [74, 62, 36]
 mountRockCol = [56, 48, 30]
 snowCol = [245, 254, 255]
 
@@ -44,92 +43,82 @@ lacunarity = 2.0
 
 thres = 0.08
 
-#Generate base noise
-
-main = numpy.zeros((gridSize,gridSize))
-
-seed = random.randint(0,200)
-
-for x in range(gridSize):
-    for y in range(gridSize):
-        main[x][y] = noise.pnoise2(x/scale,y/scale,octaves=octaves,persistence=persistance,lacunarity=lacunarity,repeatx=gridSize,repeaty=gridSize,base=seed)
-
 #Create circular gradient
 
-center_x, center_y = gridSize // 2, gridSize // 2
-circle_grad = numpy.zeros_like(main)
+center_x, center_y = gridSize // 2, gridSize // 2 #Define centre
+circle_grad = numpy.zeros((gridSize,gridSize)) #Create array
 
-for x in range(gridSize):
+for x in range(gridSize): #Loop array
     for y in range(gridSize):
-        distx = abs(x - center_x)
+        distx = abs(x - center_x) #Get distance from centre on x and y
         disty = abs(y - center_y)
-        dist = math.sqrt(distx*distx + disty*disty)
+        dist = math.sqrt(distx*distx + disty*disty) #Get the actual distance from centre (pythag)
         circle_grad[y][x] = dist
 
 max_grad = numpy.max(circle_grad)
-circle_grad = circle_grad / max_grad
+circle_grad = circle_grad / max_grad #This is some weird math that I don't quite understand but it works
 circle_grad -= 0.5
 circle_grad *= 2.0
 circle_grad = -circle_grad
 
-for y in range(gridSize):
+for y in range(gridSize): #More weird math, I think its just amplifying anything that is above 0
     for x in range(gridSize):
         if circle_grad[y][x] > 0:
             circle_grad[y][x] *= 20
 
 max_grad = numpy.max(circle_grad)
-circle_grad = circle_grad / max_grad
+circle_grad = circle_grad / max_grad #For some reason it's lowered again
 
-#Apply noise
+#Generate base noise, Apply gradient
 
+main = numpy.zeros((gridSize,gridSize)) #Init arrays
 mainNoise = numpy.zeros_like(main)
+display = numpy.zeros((gridSize,gridSize)+(3,)) #This one is set as numpy arrrays witha length of 3
+
+seed = random.randint(0,200) #Gen seed
 
 for i in range(gridSize):
     for ii in range(gridSize):
-        mainNoise[i][ii] = (main[i][ii] * circle_grad[i][ii])
+        main[i][ii] = noise.pnoise2(i/scale,ii/scale,octaves=octaves,persistence=persistance,lacunarity=lacunarity,repeatx=gridSize,repeaty=gridSize,base=seed) #Set noise
+        mainNoise[i][ii] = (main[i][ii] * circle_grad[i][ii]) #Apply gradient to noise
         if mainNoise[i][ii] > 0:
-            mainNoise[i][ii] *= 20
+            mainNoise[i][ii] *= 20 #Amplify
 
 max_grad = numpy.max(mainNoise)
-mainNoise = mainNoise / max_grad
+mainNoise = mainNoise / max_grad #Weird even out math thing
 
 #Lay base
 
-def convertCol(thres,inp,otp):
-    for i in range(0,gridSize):
-        for ii in range(0,gridSize):
-            x = inp[i][ii]
-            if x < thres + 0.015:
-                x = dwaterCol
-            elif x < thres + 0.11:
-                x = waterCol
-            elif x < thres + 0.12:
-                x = dsandCol
-            elif x < thres + 0.15:
-                x = sandCol
-            elif x < thres + 0.28:
-                x = grassCol
-            elif x < thres + 0.46:
-                x = dgrassCol
-            elif x < thres + 0.78:
-                x = dirtCol
-            elif x < thres + 1.0:
-                x = snowCol
+for i in range(0,gridSize):
+    for ii in range(0,gridSize):
+        x = mainNoise[i][ii] #Set iterator to value of main array and check if meets certain thresholds to set colours
+        if x < thres + 0.015:
+            x = dwaterCol
+        elif x < thres + 0.11:
+            x = waterCol
+        elif x < thres + 0.12:
+            x = dsandCol
+        elif x < thres + 0.15:
+            x = sandCol
+        elif x < thres + 0.28:
+            x = grassCol
+        elif x < thres + 0.46:
+            x = dgrassCol
+        elif x < thres + 0.78:
+            x = mountCol
+        elif x < thres + 1.0:
+            x = snowCol
 
-            otp[i][ii] = x
-
-display = numpy.zeros((gridSize,gridSize)+(3,))
-
-convertCol(0.08,mainNoise,display)
+        display[i][ii] = x #Set display array to the colour
 
 #Second pass (Natural features)
 
-pondSeed = random.randint(0,100)
+featSeed = random.randint(0,100) #Generate seed
 for i in range(gridSize):
     for ii in range(gridSize):
         x = display[i][ii]
-        p = noise.pnoise2(i/(scale/2.5),ii/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=pondSeed)
-        if all(x == grassCol) or all(x == dsandCol) or all(x == sandCol):
+        p = noise.pnoise2(i/(scale/2.5),ii/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=featSeed) #Get pond noise
+        if all(x == grassCol) or all(x == dsandCol) or all(x == sandCol): #If light grass or beach generate pond
             if p > 0.17:
                 if p < 0.25:
                     x = sandCol
@@ -141,21 +130,33 @@ for i in range(gridSize):
 
 def addTree(arr,x,y,inpScale):
     arr[x][y] = treeCol
-    arr[min(x+inpScale,gridSize-1)][y] = treeCol
-    arr[max(x-inpScale,0)][y] = treeCol
-    arr[x][min(y+inpScale,gridSize-1)] = treeCol
-    arr[x][max(y-inpScale,0)] = treeCol
+    n = x
+    while n < x+inpScale: #Loop through tree size (Only creates plus sign)
+        arr[min(n+1,gridSize-1)][y] = treeCol
+        n += 1
+    n = x
+    while n > x-inpScale:
+        arr[max(n-1,0)][y] = treeCol
+        n -= 1
+    n = y
+    while n < y+inpScale:
+        arr[x][min(n+1,gridSize-1)] = treeCol
+        n += 1
+    n = y
+    while n > y-inpScale:
+        arr[x][max(n-1,0)] = treeCol
+        n -= 1
 
 def addRock(arr,x,y,inpScale,c):
     arr[x][y] = c
-    arr[min(x+random.randrange(inpScale+1),gridSize-1)][y] = c
-    arr[max(x-random.randrange(inpScale+1),0)][y] = c
-    arr[x][min(y+random.randrange(inpScale+1),gridSize-1)] = c
-    arr[x][max(y-random.randrange(inpScale+1),0)] = c
+    arr[min(x+random.randint(0,1),gridSize-1)][y] = c #Random whether one is placed, if 0 is gen the origin is painted over
+    arr[max(x-random.randint(0,1),0)][y] = c
+    arr[x][min(y+random.randint(0,1),gridSize-1)] = c
+    arr[x][max(y-random.randint(0,1),0)] = c
 
 structScale = int(scale // 200)
 for i in range(gridSize):
-    for ii in range(gridSize):
+    for ii in range(gridSize): #Place rocks on beach and mountnain
         x = display[i][ii]
         if all(x == sandCol):
             if percentChance(2) == True:
@@ -166,7 +167,7 @@ for i in range(gridSize):
         elif all(x == dgrassCol):
             if percentChance(20) == True:
                 addTree(display,i,ii,structScale)
-        elif all(x == dirtCol):
+        elif all(x == mountCol):
             if percentChance(0.01) == True:
                 addRock(display,i,ii,structScale,mountRockCol)
 
