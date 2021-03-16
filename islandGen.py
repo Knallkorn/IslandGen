9d8e0f20ccfd2c579ed4d264512b9dfc0b4c6839
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-03-16 15:44:17 +1100

    Ripped Test OpenGL Stuff

--- islandGen.py
+++ islandGen.py
@@ -10,6 +10,46 @@ import sys
 
 random.seed(os.urandom(6))
 
+#OpenGL testing
+
+from OpenGL import *
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+def square():
+    glBegin(GL_QUADS)
+    glVertex2f(100,100)
+    glVertex2f(200,100)
+    glVertex2f(200,200)
+    glVertex2f(100,200)
+    glEnd()
+
+def iterate():
+    glViewport(0, 0, 500,500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Remove everything from screen (i.e. displays all white)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0,0.0,3.0)
+    square()
+    glutSwapBuffers()
+
+glutInit() # Initialize a glut instance which will allow us to customize our window
+glutInitDisplayMode(GLUT_RGBA) # Set the display mode to be colored
+glutInitWindowSize(500, 500)   # Set the width and height of your window
+glutInitWindowPosition(0, 0)   # Set the position at which this windows should appear
+wind = glutCreateWindow("Test") # Give your window a title
+glutDisplayFunc(showScreen)  # Tell OpenGL to call the showScreen method continuously
+glutIdleFunc(showScreen)     # Draw any graphics or shapes in the showScreen function at all times
+glutMainLoop()  # Keeps the window created above displaying/running in a loop
+
 #Functions
 
 def percentChance(chance):
