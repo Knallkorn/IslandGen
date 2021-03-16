6fc9605e7b7202cb789b611104ea2662c1e8a9b9
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-03-17 09:42:21 +1100

    Standardised Loops (y,x)

--- islandGen.py
+++ islandGen.py
@@ -57,8 +57,8 @@ else:
 center_x, center_y = gridSize // 2, gridSize // 2 #Define centre
 circle_grad = numpy.zeros((gridSize,gridSize)) #Create array
 
-for x in range(gridSize): #Loop array
-    for y in range(gridSize):
+for y in range(gridSize): #Loop array
+    for x in range(gridSize):
         distx = abs(x - center_x) #Get distance from centre on x and y
         disty = abs(y - center_y)
         dist = math.sqrt(distx*distx + disty*disty) #Get the actual distance from centre (pythag)
@@ -86,99 +86,99 @@ display = numpy.zeros((gridSize,gridSize)+(3,)) #This one is set as numpy arrray
 
 seed = random.randint(0,200) #Gen seed
 
-for i in range(gridSize):
-    for ii in range(gridSize):
-        main[i][ii] = noise.pnoise2(i/scale,ii/scale,octaves=octaves,persistence=persistance,lacunarity=lacunarity,repeatx=gridSize,repeaty=gridSize,base=seed) #Set noise
-        mainNoise[i][ii] = (main[i][ii] * circle_grad[i][ii]) #Apply gradient to noise
-        if mainNoise[i][ii] > 0:
-            mainNoise[i][ii] *= 20 #Amplify
+for y in range(gridSize):
+    for x in range(gridSize):
+        main[y][x] = noise.pnoise2(y/scale,x/scale,octaves=octaves,persistence=persistance,lacunarity=lacunarity,repeatx=gridSize,repeaty=gridSize,base=seed) #Set noise
+        mainNoise[y][x] = (main[y][x] * circle_grad[y][x]) #Apply gradient to noise
+        if mainNoise[y][x] > 0:
+            mainNoise[y][x] *= 20 #Amplify
 
 max_grad = numpy.max(mainNoise)
 mainNoise = mainNoise / max_grad #Weird even out math thing
 
 #Lay base
 
-for i in range(0,gridSize):
-    for ii in range(0,gridSize):
-        x = mainNoise[i][ii] #Set iterator to value of main array and check if meets certain thresholds to set colours
-        if x < thres + 0.015:
-            x = dwaterCol
-        elif x < thres + 0.11:
-            x = waterCol
-        elif x < thres + 0.12:
-            x = dsandCol
-        elif x < thres + 0.15:
-            x = sandCol
-        elif x < thres + 0.28:
-            x = grassCol
-        elif x < thres + 0.46:
-            x = dgrassCol
-        elif x < thres + 0.78:
-            x = mountCol
-        elif x < thres + 1.0:
-            x = snowCol
+for y in range(0,gridSize):
+    for x in range(0,gridSize):
+        m = mainNoise[y][x] #Set iterator to value of main array and check if meets certain thresholds to set colours
+        if m < thres + 0.015:
+            m = dwaterCol
+        elif m < thres + 0.11:
+            m = waterCol
+        elif m < thres + 0.12:
+            m = dsandCol
+        elif m < thres + 0.15:
+            m = sandCol
+        elif m < thres + 0.28:
+            m = grassCol
+        elif m < thres + 0.46:
+            m = dgrassCol
+        elif m < thres + 0.78:
+            m = mountCol
+        elif m < thres + 1.0:
+            m = snowCol
 
-        display[i][ii] = x #Set display array to the colour
+        display[y][x] = m #Set display array to the colour
 
 #Second pass (Natural features)
 
 featSeed = random.randint(0,100) #Generate seed
-for i in range(gridSize):
-    for ii in range(gridSize):
-        x = display[i][ii]
-        p = noise.pnoise2(i/(scale/2.5),ii/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=featSeed) #Get pond noise
-        if all(x == grassCol) or all(x == dsandCol) or all(x == sandCol): #If light grass or beach generate pond
+for y in range(gridSize):
+    for x in range(gridSize):
+        m = display[y][x]
+        p = noise.pnoise2(y/(scale/2.5),x/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=featSeed) #Get pond noise
+        if all(m == grassCol) or all(m == dsandCol) or all(m == sandCol): #If light grass or beach generate pond
             if p > 0.17:
                 if p < 0.25:
-                    x = sandCol
+                    m = sandCol
                 elif p < 1.0:
-                    x = waterCol
-        display[i][ii] = x
+                    m = waterCol
+        display[y][x] = m
 
 #Third pass (Structures)
 
 def addTree(arr,x,y,inpScale):
-    arr[x][y] = treeCol
-    n = x
-    while n < x+inpScale: #Loop through tree size (Only creates plus sign)
-        arr[min(n+1,gridSize-1)][y] = treeCol
-        n += 1
-    n = x
-    while n > x-inpScale:
-        arr[max(n-1,0)][y] = treeCol
-        n -= 1
+    arr[y][x] = treeCol
     n = y
-    while n < y+inpScale:
-        arr[x][min(n+1,gridSize-1)] = treeCol
+    while n < y+inpScale: #Loop through tree size (Only creates plus sign)
+        arr[min(n+1,gridSize-1)][x] = treeCol
         n += 1
     n = y
     while n > y-inpScale:
-        arr[x][max(n-1,0)] = treeCol
+        arr[max(n-1,0)][x] = treeCol
+        n -= 1
+    n = x
+    while n < x+inpScale:
+        arr[y][min(n+1,gridSize-1)] = treeCol
+        n += 1
+    n = x
+    while n > x-inpScale:
+        arr[y][max(n-1,0)] = treeCol
         n -= 1
 
 def addRock(arr,x,y,inpScale,c):
-    arr[x][y] = c
-    arr[min(x+random.randint(0,1),gridSize-1)][y] = c #Random whether one is placed, if 0 is gen the origin is painted over
-    arr[max(x-random.randint(0,1),0)][y] = c
-    arr[x][min(y+random.randint(0,1),gridSize-1)] = c
-    arr[x][max(y-random.randint(0,1),0)] = c
+    arr[y][x] = c
+    arr[min(y+random.randint(0,1),gridSize-1)][x] = c #Random whether one is placed, if 0 is gen the origin is painted over
+    arr[max(y-random.randint(0,1),0)][x] = c
+    arr[y][min(x+random.randint(0,1),gridSize-1)] = c
+    arr[y][max(x-random.randint(0,1),0)] = c
 
 structScale = int(scale // 200)
-for i in range(gridSize):
-    for ii in range(gridSize): #Place rocks on beach and mountnain
-        x = display[i][ii]
-        if all(x == sandCol):
+for y in range(gridSize):
+    for x in range(gridSize): #Place rocks on beach and mountnain
+        m = display[y][x]
+        if all(m == sandCol):
             if percentChance(2) == True:
-                addRock(display,i,ii,structScale,rockCol)
-        elif all(x == grassCol):
+                addRock(display,x,y,structScale,rockCol)
+        elif all(m == grassCol):
             if percentChance(5) == True:
-                addTree(display,i,ii,structScale)
-        elif all(x == dgrassCol):
+                addTree(display,x,y,structScale)
+        elif all(m == dgrassCol):
             if percentChance(20) == True:
-                addTree(display,i,ii,structScale)
-        elif all(x == mountCol):
+                addTree(display,x,y,structScale)
+        elif all(m == mountCol):
             if percentChance(0.01) == True:
-                addRock(display,i,ii,structScale,mountRockCol)
+                addRock(display,x,y,structScale,mountRockCol)
 
 #Show image
 
