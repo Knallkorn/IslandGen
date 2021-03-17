9a85d5de5f4e677f2f317d1165d5d4179e4de79c
Popcorn05 <flynn@rewill.net>
2021-03-17 18:04:32 +1100

    Implemented Manual Relative Path

--- chunks.py
+++ chunks.py
@@ -1,6 +1,10 @@
-import os
+import os, sys
+from pathlib import Path
 if os.path.exists("world/0_0.chunk"):
     os.remove("world/0_0.chunk")
+
+p = str(Path(__file__).parent.absolute())
+os.chdir(p)
 
 c = open("world/0_0.chunk", "w")
 for y in range(16):
