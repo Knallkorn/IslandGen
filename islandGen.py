c57e69703a4e0be1082e19048cc17df04d8d29f1
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-03-19 09:28:00 +1100

    Updated for second and third pass to work with chunks

--- islandGen.py
+++ islandGen.py
@@ -24,6 +24,35 @@ def percentChance(chance):
 def mapVal(inp, inpMin, inpMax, outMin, outMax):
     return (inp - inpMin) * (outMax - outMin) / (inpMax - inpMin) + outMin
 
+def createCircleGrad(gridSize): #Obsolete
+    #Create circular gradient (Obsolete)
+
+    center_x, center_y = gridSize // 2, gridSize // 2 #Define centre
+    circle_grad = numpy.zeros((gridSize,gridSize)) #Create array
+
+    for y in range(gridSize): #Loop array
+        for x in range(gridSize):
+            distx = abs(x - center_x) #Get distance from centre on x and y
+            disty = abs(y - center_y)
+            dist = math.sqrt(distx*distx + disty*disty) #Get the actual distance from centre (pythag)
+            circle_grad[y][x] = dist
+
+    max_grad = numpy.max(circle_grad)
+    circle_grad = circle_grad / max_grad #This is some weird math that I don't quite understand but it works
+    circle_grad -= 0.5
+    circle_grad *= 2.0
+    circle_grad = -circle_grad
+
+    for y in range(gridSize): #More weird math, I think its just amplifying anything that is above 0
+        for x in range(gridSize):
+            if circle_grad[y][x] > 0:
+                circle_grad[y][x] *= 20
+
+    max_grad = numpy.max(circle_grad)
+    circle_grad = circle_grad / max_grad #For some reason it's lowered again
+
+    return(circle_grad)
+
 #Colours
 
 dwaterCol = [54, 137, 245]
@@ -56,33 +85,7 @@ else:
     persistance = 0.5
     lacunarity = 2.0
     thres = 0.08
-"""
-#Create circular gradient (Obsolete)
 
-center_x, center_y = gridSize // 2, gridSize // 2 #Define centre
-circle_grad = numpy.zeros((gridSize,gridSize)) #Create array
-
-for y in range(gridSize): #Loop array
-    for x in range(gridSize):
-        distx = abs(x - center_x) #Get distance from centre on x and y
-        disty = abs(y - center_y)
-        dist = math.sqrt(distx*distx + disty*disty) #Get the actual distance from centre (pythag)
-        circle_grad[y][x] = dist
-
-max_grad = numpy.max(circle_grad)
-circle_grad = circle_grad / max_grad #This is some weird math that I don't quite understand but it works
-circle_grad -= 0.5
-circle_grad *= 2.0
-circle_grad = -circle_grad
-
-for y in range(gridSize): #More weird math, I think its just amplifying anything that is above 0
-    for x in range(gridSize):
-        if circle_grad[y][x] > 0:
-            circle_grad[y][x] *= 20
-
-max_grad = numpy.max(circle_grad)
-circle_grad = circle_grad / max_grad #For some reason it's lowered again
-"""
 #Generate base noise, Apply gradient
 
 im = Image.open("gradient/circle_grad.png")
@@ -137,89 +140,70 @@ for cy in range(gridSize//16):
                     passOver[cy][cx] = True
                 display[cy][cx][y][x] = m
 
-toimage(chk.Chunks.readChunkArray(gridSize,display)).show()
-
-"""
-for y in range(0,gridSize):
-    for x in range(0,gridSize):
-        m = mainNoise[y][x] #Set iterator to value of main array and check if meets certain thresholds to set colours
-        if m < thres + 0.015:
-            m = dwaterCol
-        elif m < thres + 0.11:
-            m = waterCol
-        elif m < thres + 0.12:
-            m = dsandCol
-        elif m < thres + 0.15:
-            m = sandCol
-        elif m < thres + 0.28:
-            m = grassCol
-        elif m < thres + 0.46:
-            m = dgrassCol
-        elif m < thres + 0.78:
-            m = mountCol
-        elif m < thres + 1.0:
-            m = snowCol
-
-        display[y][x] = m #Set display array to the colour
-
-
 #Second pass (Natural features)
 
 featSeed = random.randint(0,100) #Generate seed
-for y in range(gridSize):
-    for x in range(gridSize):
-        m = display[y][x]
-        p = noise.pnoise2(y/(scale/2.5),x/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=featSeed) #Get pond noise
-        if all(m == grassCol) or all(m == dsandCol) or all(m == sandCol): #If light grass or beach generate pond
-            if p > 0.17:
-                if p < 0.25:
-                    m = sandCol
-                elif p < 1.0:
-                    m = waterCol
-        display[y][x] = m
+for cy in range(gridSize//16):
+    for cx in range(gridSize//16):
+        if passOver[cy][cx] == True:
+            for y in range(16):
+                for x in range(16):
+                    m = display[cy][cx][y][x]
+                    p = noise.pnoise2((y + (cy * 16))/(scale/2.5),(x + (cx * 16))/(scale/2.5),octaves=10,persistence=0.55,lacunarity=1.55,repeatx=gridSize,repeaty=gridSize,base=featSeed) #Get pond noise
+                    if all(m == grassCol) or all(m == dsandCol) or all(m == sandCol): #If light grass or beach generate pond
+                        if p > 0.17:
+                            if p < 0.25:
+                                m = sandCol
+                            elif p < 1.0:
+                                m = waterCol
+                    display[cy][cx][y][x] = m
 
 #Third pass (Structures)
 
-def addTree(arr,x,y,inpScale):
-    arr[y][x] = treeCol
+def addTree(arr,cx,cy,x,y,inpScale):
+    arr[cy][cx][y][x] = treeCol
     n = y
     while n < y+inpScale: #Loop through tree size (Only creates plus sign)
-        arr[min(n+1,gridSize-1)][x] = treeCol
+        arr[cy][cx][min(n+1,15)][x] = treeCol
         n += 1
     n = y
     while n > y-inpScale:
-        arr[max(n-1,0)][x] = treeCol
+        arr[cy][cx][max(n-1,0)][x] = treeCol
         n -= 1
     n = x
     while n < x+inpScale:
-        arr[y][min(n+1,gridSize-1)] = treeCol
+        arr[cy][cx][y][min(n+1,15)] = treeCol
         n += 1
     n = x
     while n > x-inpScale:
-        arr[y][max(n-1,0)] = treeCol
+        arr[cy][cx][y][max(n-1,0)] = treeCol
         n -= 1
 
-def addRock(arr,x,y,inpScale,c):
-    arr[y][x] = c
-    arr[min(y+random.randint(0,1),gridSize-1)][x] = c #Random whether one is placed, if 0 is gen the origin is painted over
-    arr[max(y-random.randint(0,1),0)][x] = c
-    arr[y][min(x+random.randint(0,1),gridSize-1)] = c
-    arr[y][max(x-random.randint(0,1),0)] = c
+def addRock(arr,cx,cy,x,y,inpScale,c):
+    arr[cy][cx][y][x] = c
+    arr[cy][cx][min(y+random.randint(0,1),15)][x] = c #Random whether one is placed, if 0 is gen the origin is painted over
+    arr[cy][cx][max(y-random.randint(0,1),0)][x] = c
+    arr[cy][cx][y][min(x+random.randint(0,1),15)] = c
+    arr[cy][cx][y][max(x-random.randint(0,1),0)] = c
 
 structScale = int(scale // 200)
-for y in range(gridSize):
-    for x in range(gridSize): #Place rocks on beach and mountnain
-        m = display[y][x]
-        if all(m == sandCol):
-            if percentChance(2) == True:
-                addRock(display,x,y,structScale,rockCol)
-        elif all(m == grassCol):
-            if percentChance(5) == True:
-                addTree(display,x,y,structScale)
-        elif all(m == dgrassCol):
-            if percentChance(20) == True:
-                addTree(display,x,y,structScale)
-        elif all(m == mountCol):
-            if percentChance(0.01) == True:
-                addRock(display,x,y,structScale,mountRockCol)
-"""
+for cy in range(gridSize//16):
+    for cx in range(gridSize//16):
+        if passOver[cy][cx] == True:
+            for y in range(16):
+                for x in range(16): #Place rocks on beach and mountnain
+                    m = display[cy][cx][y][x]
+                    if all(m == sandCol):
+                        if percentChance(2) == True:
+                            addRock(display,cx,cy,x,y,structScale,rockCol)
+                    elif all(m == grassCol):
+                        if percentChance(5) == True:
+                            addTree(display,cx,cy,x,y,structScale)
+                    elif all(m == dgrassCol):
+                        if percentChance(20) == True:
+                            addTree(display,cx,cy,x,y,structScale)
+                    elif all(m == mountCol):
+                        if percentChance(0.01) == True:
+                            addRock(display,cx,cy,x,y,structScale,mountRockCol)
+
+toimage(chk.Chunks.readChunkArray(gridSize,display)).show()
